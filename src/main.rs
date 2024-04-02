e5029d6910b971c75913c1991aad9f689125d2e1
suphakin th <suphakin.th@gmail.com>
2024-04-02 22:56:18 +0700

    Feat: Just learn it session 2.

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn main() {
+    let second_variable = 999;
+
+    println!("second_variable is {}", second_variable);
+    let mut _a_number = 1;
+    _a_number = 2;
+    println!("a_number is {}", _a_number);
+
+    let x:i32 = 1;
+    println!("1 in i32? {} {}", type_of(x), x);
+
+    let y:u32 = 14;
+    println!("14 in u32? {} {}", type_of(y), y);
+
+    let thailand = 3.0;
+    println!("3.0 in normal let? {} {}", type_of(thailand), thailand);
+
+    let thailand_f:f32 = 4.0;
+    println!("4.0 in f32? {} {}", type_of(thailand_f), thailand_f);
+
+    let auu = (2 % 2) == 0;
+    tracing::info!("2 is odd? {:?}", auu);
+}
