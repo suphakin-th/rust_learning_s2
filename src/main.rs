a6f936a998154e98b4e31ad40c107f7ac332d12a
suphakin th <suphakin.th@gmail.com>
2024-04-05 18:23:44 +0700

    Feature : Add some condition.

--- src/main.rs
+++ src/main.rs
@@ -83,4 +83,15 @@ fn main() {
     let arr: [i32; 3] = [1, 2, 3];
     println!("{:?}", arr);
     println!("{}", arr[1]);
+
+    // basic for control Flow.
+    let x = 42;
+
+    if x < 42 {
+        println!("less than 42");
+    } else if x == 42 {
+        println!("is 42");
+    } else {
+        println!("greater than 42");
+    }
 }
